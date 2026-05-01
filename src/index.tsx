810e1e54cfbf5b6fe62f32bab33def695f439f80
macbook <macbook@macbooks-MacBook-Pro.local>
2026-05-01 11:17:41 +0300

    first clean

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import Demo from './Demo';
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <Demo />
+);
